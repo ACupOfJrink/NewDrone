42e2d719b5a58694f29ed1c6b6a364cf9495f220
ACupOfJrink <2023331582@qq.com>
2025-04-26 12:09:26 +0800

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -44,17 +44,21 @@ st.markdown('<div class="center"><img src="data:image/png;base64,' + image_to_ba
 
 #引入空格换行
 st.markdown(" ")
+st.markdown('<div class="center"><img src="data:image1/png;base64,' + image_to_base64(image1) + '" width="500"></div>', unsafe_allow_html=True)
+st.markdown('<div class="center"><img src="data:image2/png;base64,' + image_to_base64(image1) + '" width="500"></div>', unsafe_allow_html=True)
 
-# 展示图片
-st.image(image1, caption='Image 1', width=500)
-st.image(image2, caption='Image 2', width=500)
+
 
 
 st.title("队形算法设计")
 
 st.header("目标问题")
-st.markdown('''假设队形变换在无障碍物情况下进行变换，则无人机 从 到 的所有路线中直线路径最短，那么问题就可以将队形变化变成优化问题。该优化问题需要求解在最小时间内的队形变化方案，该优化问题实际上是一个指派问题。若编队中有 架飞机，则潜在的指派方案就有 种。目标函数为能量耗费 和使用时间 的加权平均，则优化问题的数学表达式为''')
-st.markdown(r'''$$min\;\;J()=\alhpa E(k)+(1-\alpha) \textit{T}(k) \\ \begin{cases} \textit{E}(k) = \sum^{n}_{i=1} \textit{E_i}(k) \\ \textit{T}(k)=max\{ t_1(k),t_2(k), \codts , t_n(k) , \alpha\in [0,1]\} \end{cases}}$$  ''')
+st.markdown('''假设队形变换在无障碍物情况下进行变换，则无人机p从q到的所有路线中直线路径最短，那么问题就可以将队形变化变成优化问题。该优化问题需要求解在最小时间内的队形变化方案，该优化问题实际上是一个指派问题。若编队中有 架飞机，则潜在的指派方案就有 种。目标函数为能量耗费 和使用时间 的加权平均，则优化问题的数学表达式为''')
+st.latex(r'''minJ(k, \zeta)=\alpha E(k)+(1-\alpha) \textit{T}(k) \\ \begin{cases} 
+\textit{E}(k) = \textit{E}(k) = \sum^{n}_{i=1} \textit{E}(k) 
+ \\ 
+\textit{T}(k)=max\{ t_1(k),t_2(k), \cdots , t_{n(k)} , \alpha \in [0,1]\}
+ \end{cases}''')
 
 
 
@@ -74,7 +78,7 @@ st.markdown(
 )
 
 # 使用 st.markdown() 或 st.latex() 来显示公式
-st.markdown('<div class="center">$$E = mc^2$$</div>', unsafe_allow_html=True)
+
 
 
 
